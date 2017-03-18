4a92fb109a8be012fd931189709a4157358d4a85
Alexey Nabrodov <alexey.nabrodov@team.wrike.com>
2017-03-18 21:02:20 +0300

    init

---
+++ tripod.h
@@ -0,0 +1,17 @@
+#ifndef TRIPOD_H
+#define TRIPOD_H
+
+#include "quantum.h"
+
+// This a shortcut to help you visually see your layout.
+// The following is an example using the Planck MIT layout
+// The first section contains all of the arguements
+// The second converts the arguments into a two-dimensional array
+#define KEYMAP( \
+    k00, k01, k02, \
+) \
+{ \
+    { k00, k01,   k02 }, \
+}
+
+#endif
